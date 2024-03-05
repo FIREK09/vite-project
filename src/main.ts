556453d1372da37ae0eee2157a5b44c17740853e
FIREK09 <vovandosina_11@mail.ru>
2024-03-05 21:17:01 +0300

    42342352

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 import './style.sass'
+ 
+let num1 = 7;
+let num2 = 5;
+
+function findSmallerNumber(num1:number, num2:number) {
+    return Math.min(num1, num2);
+}
+console.log(findSmallerNumber(num1, num2));
+
+
